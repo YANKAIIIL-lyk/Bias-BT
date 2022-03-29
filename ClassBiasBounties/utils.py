97eca62f75830dc455d0cec6d8d9424239fba070
YANKAIIIL-lyk <yankail@seas.upenn.edu>
2022-03-29 00:22:20 -0400

    refactor for data wrangling

--- ClassBiasBounties/utils.py
+++ ClassBiasBounties/utils.py
@@ -6,6 +6,7 @@ from dontlook import bountyHuntData
 from dontlook import bountyHuntWrapper
 import uuid
 from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import OneHotEncoder
 
 import numpy as np
 import pandas as pd
@@ -240,6 +241,161 @@ def find_top_N_initial_g(N, dataset_x, dataset_y, initial_model, groups):
 #
 #         groups.append(g)
 #
+
+'''
+This method will preprocess the training set and validation set. Drop all the outliers first. For the categorical
+features, do one-hot encoding
+'''
+def preprocess(train_x, validation_x):
+    # AGEP(age) - Numerical data, no preprocessing is needed
+    # SCHL(Educational attainment) - Categorical data, but it's okay to preserve the natural order here
+
+    # MAR(Marital status) - Categorical data, 85% of data is 1 or 5.
+    # TODO: Need one hot encoding
+    def preprocess_SEX(x):
+        if (x == 1):
+            return 1
+        return 0
+
+    train_x['SEX'] = train_x['SEX'].apply(preprocess_SEX)
+    validation_x['SEX'] = validation_x['SEX'].apply(preprocess_SEX)
+
+    # DIS(Disability recode) - binary data. Return 1 if with a disability, otherwise return 0.
+    def preprocess_DIS(x):
+        if (x == 1):
+            return 1
+        return 0
+
+    train_x['DIS'] = train_x['DIS'].apply(preprocess_DIS)
+    validation_x['DIS'] = validation_x['DIS'].apply(preprocess_DIS)
+
+    # ESP(Employment status of parents) - 82% is 0, 8.3% is 1, all the other data ranges from 2 - 8
+    # TODO: Need one hot encoding
+    def preprocess_ESP(x):
+        if (x != 0 and x != 1):
+            return 2
+        return x
+
+    train_x['ESP'] = train_x['ESP'].apply(preprocess_ESP)
+    validation_x['ESP'] = validation_x['ESP'].apply(preprocess_ESP)
+
+    # MIG(Mobility status) - 88.5% is 1, 9.9% is 3. Since the other data only makes up to 1% of whole data set,
+    # merge it into category 3. Then this feature becomes a binary feature.
+    # Return 1 if original data is 1, otherwise return 0
+    def preprocess_MIG(x):
+        if (x == 1):
+            return 1
+        return 0
+
+    train_x['MIG'] = train_x['MIG'].apply(preprocess_MIG)
+    validation_x['MIG'] = validation_x['MIG'].apply(preprocess_MIG)
+
+    # CIT(Citizenship status) - 78% data is 1, 12.2% data is 4. Other data will be merged.
+    # return 1 -> 0, 4 -> 1, others -> 2
+    # TODO: Need one hot encoding
+    def preprocess_CIT(x):
+        if (x == 1):
+            return 0
+        elif (x == 4):
+            return 1
+        return 2
+
+    train_x['CIT'] = train_x['CIT'].apply(preprocess_CIT)
+    validation_x['CIT'] = validation_x['CIT'].apply(preprocess_CIT)
+
+    # MIL(Military service) - 76.7% data is 4, 17.8% data is 0. Other data will be merged.
+    # return 0 -> 0, 4 -> 1, others -> 2
+    # TODO: Need one hot encoding
+    def preprocess_MIL(x):
+        if (x == 0):
+            return 0
+        elif (x == 4):
+            return 1
+        return 2
+
+    train_x['MIL'] = train_x['MIL'].apply(preprocess_MIL)
+    validation_x['MIL'] = validation_x['MIL'].apply(preprocess_MIL)
+
+    # ANC(Ancestry recode) - 1.6% data is 3, will be merged into 4
+    # TODO: Need one hot encoding
+    def preprocess_ANC(x):
+        if (x == 3):
+            return 4
+        return x
+
+    train_x['ANC'] = train_x['ANC'].apply(preprocess_ANC)
+    validation_x['ANC'] = validation_x['ANC'].apply(preprocess_ANC)
+
+    # NATIVITY(Nativity) - binary data. Return 1 if Native
+    def preprocess_NATIVITY(x):
+        if (x == 1):
+            return 0
+        return 1
+
+    train_x['NATIVITY'] = train_x['NATIVITY'].apply(preprocess_NATIVITY)
+    validation_x['NATIVITY'] = validation_x['NATIVITY'].apply(preprocess_NATIVITY)
+
+    # RELP(Relationship) - 38.3% is 0, 24.5% is 2, 18.5% is 1, all the other values are below 3.5%. Merge them into
+    # category 3
+    # TODO: Need one hot encoding
+    def preprocess_RELP(x):
+        if (x not in [0, 1, 2]):
+            return 3
+        return x
+
+    train_x['RELP'] = train_x['RELP'].apply(preprocess_RELP)
+    validation_x['RELP'] = validation_x['RELP'].apply(preprocess_RELP)
+
+    # DEAR, DEYE - binary data. Return 1 if Yes(1), otherwise 0.
+    def preprocess_DEAR(x):
+        if (x == 1):
+            return 1
+        return 0
+
+    def preprocess_DEYE(x):
+        return preprocess_DEAR(x)
+
+    train_x['DEAR'] = train_x['DEAR'].apply(preprocess_DEAR)
+    train_x['DEYE'] = train_x['DEYE'].apply(preprocess_DEYE)
+    validation_x['DEAR'] = validation_x['DEAR'].apply(preprocess_DEAR)
+    validation_x['DEYE'] = validation_x['DEYE'].apply(preprocess_DEYE)
+
+    # DREM(Cognitive difficulty) - No preprocess needed
+    # TODO: Need one hot encoding
+
+    # RAC1P(Recoded detailed race code) - category 1, 2, 6, 8 includes 95% of the data. So all the other data will be
+    # categorized into one category
+    # TODO: Need one hot encoding
+    def preprocess_RAC1P(x):
+        if (x in [9, 3, 5, 7, 4]):
+            return 0
+        return x
+
+    train_x['RAC1P'] = train_x['RAC1P'].apply(preprocess_RAC1P)
+    validation_x['RAC1P'] = validation_x['RAC1P'].apply(preprocess_RAC1P)
+
+    # make sure all the data are int
+    train_x = pd.DataFrame(train_x, dtype=int)
+    validation_x = pd.DataFrame(validation_x, dtype=int)
+
+    # one-hot encode the training
+    features_to_encode = ['MAR', 'ESP', 'CIT', 'MIL', 'ANC', 'RELP', 'DREM', 'RAC1P']
+    encoder = OneHotEncoder(handle_unknown='ignore')
+    train_x_encoded = pd.DataFrame(encoder.fit_transform(train_x[features_to_encode]).toarray(), \
+                                   columns=encoder.get_feature_names_out(features_to_encode))
+    train_x = train_x.drop(columns=features_to_encode, axis=1)
+    train_x = train_x.join(train_x_encoded)
+    train_x = pd.DataFrame(train_x, dtype=int)
+
+    # one-hot encode the validation
+    encoder = OneHotEncoder(handle_unknown='ignore')
+    validation_x_encoded = pd.DataFrame(encoder.fit_transform(validation_x[features_to_encode]).toarray(), \
+                                        columns=encoder.get_feature_names_out(features_to_encode))
+    validation_x = validation_x.drop(columns=features_to_encode, axis=1)
+    validation_x = validation_x.join(validation_x_encoded)
+    validation_x = pd.DataFrame(validation_x, dtype=int)
+
+    return train_x, validation_x
 
 if __name__ == '__main__':
     # test
